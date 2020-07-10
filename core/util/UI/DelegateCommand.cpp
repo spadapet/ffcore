a70e4e47ffb47a81296ef2db429d05cad36f0c60
Peter Spada <peterspa@microsoft.com>
2020-07-10 11:15:53 -0700

    DelegateCommand

--- core/util/UI/DelegateCommand.cpp
+++ core/util/UI/DelegateCommand.cpp
@@ -1,20 +1,24 @@
 #include "pch.h"
 #include "UI/DelegateCommand.h"
 
+NS_IMPLEMENT_REFLECTION(ff::DelegateCommand)
+{
+}
+
 ff::DelegateCommand::DelegateCommand(ExecuteFunc&& execute)
     : _execute(std::move(execute))
 {
 }
 
-ff::DelegateCommand::DelegateCommand(ExecuteFunc&& execute, CanExecuteFunc&& canExecute)
-    : _execute(std::move(execute))
-    , _canExecute(std::move(canExecute))
+ff::DelegateCommand::DelegateCommand(CanExecuteFunc&& canExecute, ExecuteFunc&& execute)
+    : _canExecute(std::move(canExecute))
+    , _execute(std::move(execute))
 {
 }
 
 bool ff::DelegateCommand::CanExecute(Noesis::BaseComponent* param) const
 {
-    return !_canExecute || _canExecute(param);
+    return _canExecute.Empty() || _canExecute(param);
 }
 
 void ff::DelegateCommand::Execute(Noesis::BaseComponent* param) const
--- core/util/UI/DelegateCommand.h
+++ core/util/UI/DelegateCommand.h
@@ -5,17 +5,20 @@ namespace ff
     class DelegateCommand : public Noesis::BaseCommand
     {
     public:
-        typedef std::function<void(Noesis::BaseComponent*)> ExecuteFunc;
-        typedef std::function<bool(Noesis::BaseComponent*)> CanExecuteFunc;
+        typedef Noesis::Delegate<void(BaseComponent*)> ExecuteFunc;
+        typedef Noesis::Delegate<bool(BaseComponent*)> CanExecuteFunc;
 
+        UTIL_API DelegateCommand();
         UTIL_API DelegateCommand(ExecuteFunc&& execute);
-        UTIL_API DelegateCommand(ExecuteFunc&& execute, CanExecuteFunc&& canExecute);
+        UTIL_API DelegateCommand(CanExecuteFunc&& canExecute, ExecuteFunc&& execute);
 
         bool CanExecute(Noesis::BaseComponent* param) const override;
         void Execute(Noesis::BaseComponent* param) const override;
 
     private:
-        ExecuteFunc _execute;
         CanExecuteFunc _canExecute;
+        ExecuteFunc _execute;
+
+        NS_DECLARE_REFLECTION(DelegateCommand, BaseCommand)
     };
 }
